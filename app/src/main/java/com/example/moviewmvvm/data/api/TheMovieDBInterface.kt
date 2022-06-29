59e4b0c7d2b714366476f5f464df750b30ff4793
portmaler <56043999+portmaler@users.noreply.github.com>
2022-06-29 13:15:38 +0100

    first commit

---
+++ app/src/main/java/com/example/moviewmvvm/data/api/TheMovieDBInterface.kt
@@ -0,0 +1,16 @@
+package com.example.moviewmvvm.data.api
+
+import com.example.moviewmvvm.data.vo.MovieDetails
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TheMovieDBInterface {
+    // https://api.themoviedb.org/3/movie/popular?api_key=6e63c2317fbe963d76c3bdc2b785f6d1&page=1
+    // https://api.themoviedb.org/3/movie/299534?api_key=6e63c2317fbe963d76c3bdc2b785f6d1
+    // https://api.themoviedb.org/3/
+
+    @GET("movie/{movie_id}")
+    fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
+}
---
+++ app/src/main/java/com/example/moviewmvvm/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,29 @@
+package com.example.moviewmvvm.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.moviewmvvm.data.api.TheMovieDBInterface
+import com.example.moviewmvvm.data.repository.MovieDetailsNetworkDataSource
+import com.example.moviewmvvm.data.repository.NetworkState
+import com.example.moviewmvvm.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository (private val apiService : TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails (compositeDisposable: CompositeDisposable, movieId: Int) : LiveData<MovieDetails> {
+
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService,compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState> {
+        return movieDetailsNetworkDataSource.networkState
+    }
+
+
+
+}
