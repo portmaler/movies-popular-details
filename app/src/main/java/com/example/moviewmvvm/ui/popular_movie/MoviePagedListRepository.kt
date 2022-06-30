1cee4958fd38e0a0a1e71f0c4b4ac3b95bacab53
portmaler <56043999+portmaler@users.noreply.github.com>
2022-06-30 16:56:53 +0100

    third and last commit

---
+++ app/src/main/java/com/example/moviewmvvm/ui/popular_movie/MoviePagedListRepository.kt
@@ -0,0 +1,37 @@
+package com.example.moviewmvvm.ui.popular_movie
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.moviewmvvm.data.api.POST_PER_PAGE
+import com.example.moviewmvvm.data.api.TheMovieDBInterface
+import com.example.moviewmvvm.data.repository.MovieDataSource
+import com.example.moviewmvvm.data.repository.MovieDataSourceFactory
+import com.example.moviewmvvm.data.repository.NetworkState
+import com.example.moviewmvvm.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MoviePagedListRepository (private val apiService : TheMovieDBInterface) {
+
+    lateinit var moviePagedList: LiveData<PagedList<Movie>>
+    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+
+    fun fetchLiveMoviePagedList (compositeDisposable: CompositeDisposable) : LiveData<PagedList<Movie>> {
+        moviesDataSourceFactory = MovieDataSourceFactory(apiService, compositeDisposable)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setPageSize(POST_PER_PAGE)
+            .build()
+
+        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory, config).build()
+
+        return moviePagedList
+    }
+
+    fun getNetworkState(): LiveData<NetworkState> {
+        return Transformations.switchMap<MovieDataSource, NetworkState>(
+            moviesDataSourceFactory.moviesLiveDataSource, MovieDataSource::networkState)
+    }
+}
